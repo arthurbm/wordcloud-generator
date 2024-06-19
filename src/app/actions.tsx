d1f4a552e2902bca731154470f9adba11ce35568
arthur brito <arthurmedeiros32@gmail.com>
2024-06-19 16:44:42 -0300

    feat: make prompt more organized

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -11,7 +11,7 @@ import { type ModelName } from "~/types";
 export async function getWords(
   text: string,
   modelName: ModelName,
-  blackcklistWords?: string,
+  blacklistWords?: string,
 ) {
   // const ollama = createOllama({
   //   baseURL: "http://213.163.246.171:40002/api",
@@ -37,27 +37,36 @@ export async function getWords(
       {
         role: "system",
         content: `
-        Your task is to analyze the text provided by the user and list between 35 and 40 of the main keywords with semantic value for the context.
-        If there are between 35 and 40 words, I will give you a $200 tip. If not, you will be fired and replaced by another AI that can do the job better.
-        You must focus exclusively on nouns and expressions, eliminating all other classes of words such as adjectives, verbs, adverbs, pronouns, articles, numerals, prepositions, conjunctions, and interjections.
-        Extract only the nouns that are essential to capture the main themes or central ideas of the text.
-        Provide the list of identified keywords as nouns separated by commas.
-        Never number the list of words.
-        Accentuate the words correctly.
-        Compound words are considered as a single word. For example, "data analysis" is a single word. Do not separate them. If they are valuable, include them in the list.
-        The words must appear in order of importance.
-        Theh words must be in the same language as the text.
-        Avoid nouns with little semantic value such as "table", "document", "participant", "conference", "debate", etc.
-        Do not repeat words.
-        Do not put a dot, comma or whitespace at the end of the list.
-        Do not include company, organizations and people's names.
-        ${blackcklistWords ? `Do not include these words on your answer: ${blackcklistWords}` : ""}
+        Sua tarefa é analisar o texto fornecido pelo usuário e identificar entre 15 a 20 das principais palavras-chave com valor semântico para o contexto. 
 
-        Example:
+        ### Critérios:
+        1. **Foco Exclusivo em Substantivos e Expressões**:
+          - Elimine todas as outras classes de palavras (adjetivos, verbos, advérbios, pronomes, artigos, numerais, preposições, conjunções e interjeições).
+          - Concentre-se apenas em substantivos essenciais que capturam os principais temas ou ideias centrais do texto.
+          
+        2. **Formato da Resposta**:
+          - Forneça a lista de palavras-chave identificadas como substantivos separados por vírgulas.
+          - Não numere a lista de palavras.
+          - As palavras devem estar acentuadas corretamente.
+          - Palavras compostas são consideradas como uma única palavra (por exemplo, "análise de dados").
+          - A lista deve estar em ordem de importância.
+          - As palavras devem estar no mesmo idioma do texto.
+          
+        3. **Eliminação de Substantivos de Baixo Valor Semântico**:
+          - Evite palavras como "mesa", "documento", "participante", "conferência", "debate", etc.
+          
+        4. **Exclusões Específicas**:
+          - Não inclua lugares, empresas, organizações ou nomes de pessoas (por exemplo, "Brasil" ,"Paris", "ONU", "Bill Gates", "Departamento de ciência", "NASA").
+          - Não repita palavras.
+          - Não coloque ponto, vírgula ou espaço no final da lista.
 
-        User Text: "A conferência internacional sobre mudanças climáticas, realizada em Paris, reuniu cientistas, políticos e ativistas de todo o mundo para discutir soluções para os problemas causados pelo aquecimento global. Os participantes destacaram os impactos devastadores nas regiões costeiras, como o aumento do nível do mar e a frequência de tempestades severas. Foram apresentados estudos sobre a degradação dos ecossistemas marinhos e terrestres, além da perda de biodiversidade. A importância de implementar práticas agrícolas sustentáveis foi amplamente debatida, com ênfase na redução do desmatamento e na promoção de técnicas de cultivo que preservem o solo e a água. Outro ponto crucial foi o papel das energias renováveis, como a solar e a eólica, na diminuição das emissões de carbono. Os especialistas sugeriram políticas públicas para incentivar a adoção dessas tecnologias e a necessidade de investimentos em pesquisa e desenvolvimento. A análise de dados recentes mostrou um aumento alarmante na concentração de gases de efeito estufa na atmosfera, reforçando a urgência de medidas concretas. As estratégias propostas incluíram desde reformas econômicas até a mobilização de comunidades locais para ações de mitigação e adaptação. O relatório final destacou a necessidade de uma cooperação internacional robusta e contínua para enfrentar os desafios impostos pelas mudanças climáticas e garantir um futuro sustentável para as próximas gerações."
+        5. **Considerações Adicionais**:
+          ${blacklistWords ? `- NÃO inclua estas palavras na sua resposta: ${blacklistWords}` : ""}
 
-        Response: "mudanças climáticas, Paris, cientistas, políticos, ativistas, soluções, problemas, aquecimento global, participantes, impactos, regiões costeiras, aumento do nível do mar, tempestades, estudos, degradação, ecossistemas marinhos, terrestres, perda de biodiversidade, práticas agrícolas sustentáveis, desmatamento, técnicas de cultivo, solo, água, energias renováveis, solar, eólica, emissões de carbono, políticas públicas, tecnologias, investimentos, pesquisa, desenvolvimento, gases de efeito estufa, atmosfera, medidas, reformas econômicas, mobilização"
+        ### Exemplo de Entrada e Saída:
+        - **Entrada**:"A conferência internacional sobre mudanças climáticas, realizada em Paris, reuniu cientistas, políticos e ativistas de todo o mundo para discutir soluções para os problemas causados pelo aquecimento global. Os participantes destacaram os impactos devastadores nas regiões costeiras, como o aumento do nível do mar e a frequência de tempestades severas. Foram apresentados estudos sobre a degradação dos ecossistemas marinhos e terrestres, além da perda de biodiversidade. A importância de implementar práticas agrícolas sustentáveis foi amplamente debatida, com ênfase na redução do desmatamento e na promoção de técnicas de cultivo que preservem o solo e a água. Outro ponto crucial foi o papel das energias renováveis, como a solar e a eólica, na diminuição das emissões de carbono. Os especialistas sugeriram políticas públicas para incentivar a adoção dessas tecnologias e a necessidade de investimentos em pesquisa e desenvolvimento. A análise de dados recentes mostrou um aumento alarmante na concentração de gases de efeito estufa na atmosfera, reforçando a urgência de medidas concretas. As estratégias propostas incluíram desde reformas econômicas até a mobilização de comunidades locais para ações de mitigação e adaptação. O relatório final destacou a necessidade de uma cooperação internacional robusta e contínua para enfrentar os desafios impostos pelas mudanças climáticas e garantir um futuro sustentável para as próximas gerações."
+
+        - **Saída**: "mudanças climáticas, Paris, cientistas, políticos, ativistas, soluções, problemas, aquecimento global, participantes, impactos, regiões costeiras, aumento do nível do mar, tempestades, estudos, degradação, ecossistemas marinhos, terrestres, perda de biodiversidade, práticas agrícolas sustentáveis, desmatamento, técnicas de cultivo, solo, água, energias renováveis, solar, eólica, emissões de carbono, políticas públicas, tecnologias, investimentos, pesquisa, desenvolvimento, gases de efeito estufa, atmosfera, medidas, reformas econômicas, mobilização"
         `,
       },
       {
