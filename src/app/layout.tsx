aa4bfbb70ae8a53847d9f8b3c09d2aab2707e83d
arthur brito <arthurmedeiros32@gmail.com>
2024-06-25 22:59:27 -0300

    feat: put layour back to server component and add dynamic to top of page tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
-"use client";
-
-export const dynamic = "force-dynamic";
-export const maxDuration = 300;
-
 import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { Toaster } from "sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const metadata = {
   title: "Wordcloud generator",
@@ -20,16 +14,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="en" className={`${GeistSans.variable}`}>
-        <body>
-          {children}
-          <Toaster />
-        </body>
-      </html>
-    </QueryClientProvider>
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        {children}
+        <Toaster />
+      </body>
+    </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 "use client";
+
+export const dynamic = "force-dynamic";
+export const maxDuration = 300;
+
 import { WordCloudForm } from "~/components/word-cloud-form";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function HomePage() {
+  const queryClient = new QueryClient();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
-      <WordCloudForm />
-    </main>
+    <QueryClientProvider client={queryClient}>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 dark:bg-gray-900">
+        <WordCloudForm />
+      </main>
+    </QueryClientProvider>
   );
 }
